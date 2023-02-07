e4e4392d653357c12d9c9fe46cfa623ad4951446
renzinan@gmail.com <renzinan@gmail.com>
2023-02-07 18:29:45 +0800

    update run script

--- run_all_engines.sh
+++ run_all_engines.sh
@@ -2,30 +2,38 @@
 
 set -e
 
-DATASETS=${DATASETS:-"*"}
+DATASETS=${DATASETS:-"glove-100-angular glove-25-angular gist-960-euclidean deep-image-96-angular"}
+DATASET_ARGS=""
+for dataset in $DATASETS; do
+    DATASET_ARGS="$DATASET_ARGS --datasets $dataset"
+done
 
 SERVER_HOST=${SERVER_HOST:-"localhost"}
+SERVER_USERNAME=${SERVER_USERNAME:-"user"}
 
-SERVER_USERNAME=${SERVER_USERNAME:-"qdrant"}
+SSH_OPT="-i ~/.ssh/ann-cloud-engine-benchmark_key.pem"
 
 function run_exp() {
     SERVER_PATH=$1
     ENGINE_NAME=$2
     MONITOR_PATH=$(echo $ENGINE_NAME | sed -e 's/[^A-Za-z0-9._-]/_/g')
-    ssh ${SERVER_USERNAME}@${SERVER_HOST} "nohup bash -c 'cd ./projects/vector-db-benchmark/monitoring && rm -f docker.stats.jsonl && bash monitor_docker.sh' > /dev/null 2>&1 &"
-    ssh -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d"
+    ssh $SSH_OPT ${SERVER_USERNAME}@${SERVER_HOST} "nohup bash -c 'cd ./projects/vector-db-benchmark/monitoring && rm -f docker.stats.jsonl && bash monitor_docker.sh' > /dev/null 2>&1 &"
+    ssh $SSH_OPT -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down ; docker compose up -d"
     sleep 30
-    python3 run.py --engines $ENGINE_NAME --datasets "${DATASETS}" --host $SERVER_HOST
-    ssh -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down"
-    ssh -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl"
+    python3 run.py --engines $ENGINE_NAME $DATASET_ARGS --host $SERVER_HOST
+    ssh $SSH_OPT -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/engine/servers/$SERVER_PATH ; docker compose down"
+    ssh $SSH_OPT -t ${SERVER_USERNAME}@${SERVER_HOST} "cd ./projects/vector-db-benchmark/monitoring && mkdir -p results && mv docker.stats.jsonl ./results/${MONITOR_PATH}-docker.stats.jsonl"
 }
 
 
-#run_exp "qdrant-single-node" 'qdrant-m-*'
-run_exp "weaviate-single-node" 'weaviate-m-*'
-run_exp "milvus-single-node" 'milvus-m-*'
-run_exp "qdrant-single-node" 'qdrant-rps-m-*'
+#run_exp "qdrant-single-node" 'qdrant-m-32-*'
+#run_exp "weaviate-single-node" 'weaviate-m-32-*'
+#run_exp "milvus-single-node" 'milvus-m-32-*'
 
+run_exp "weaviate-single-node" 'weaviate-m-*-ef-128'
+run_exp "milvus-single-node" 'milvus-m-*-ef-128'
+run_exp "qdrant-single-node" 'qdrant-rps-m-*-ef-128'
+#run_exp "elasticsearch-single-node" 'elastic-m-*-ef-128'
 
 # run_exp "elasticsearch-single-node" 'elastic-m-*'
 # run_exp "redis-single-node" 'redis-m-*'
